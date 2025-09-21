db4e5810456bfd71c6e817cc653eda08480163e7
merikhimo <merikhimo@gmail.com>
2025-09-21 13:30:22 +0300

    fixed

--- frontend/src/components/appbar.tsx
+++ frontend/src/components/appbar.tsx
@@ -1,14 +1,8 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import Registration from "./registration";
 
 export default function AppBar() {
     const [menu, setMenu] = useState(false);
-    const [showHello, setShowHello] = useState(false);
-
-  useEffect(() => {
-    const value = localStorage.getItem("email");
-    if (value) setShowHello(true);
-  }, []);
 
     return(
         <div className="appbar">
